b0b842bac5ba4440d35c0e48ab3c0daec6287a3d
d.baranov <d.baranov@zenit.ru>
2022-09-14 19:47:03 +0300

    fix readme

--- Chapter-2/selectionSort_test.go
+++ Chapter-2/selectionSort_test.go
@@ -7,70 +7,55 @@ import (
 )
 
 func Test_SelectionSort(t *testing.T) {
-	t.Run("errors", func(t *testing.T) {
-		t.Parallel()
-		t.Run("slice is nil", func(t *testing.T) {
-			var items []int
+	cases := []struct {
+		name string
+		arg  []int
+		want []int
+	}{
+		{
+			"slice is empty",
+			[]int{},
+			[]int{},
+		},
+		{
+			"one element",
+			[]int{1},
+			[]int{1},
+		},
+		{
+			"two element",
+			[]int{2, 1},
+			[]int{1, 2},
+		},
+		{
+			"positive elements",
+			[]int{2, 0, 5, 1, 7, 9, 6, 3, 4, 8},
+			[]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+		},
+		{
+			"negative elements",
+			[]int{-1, -2, -3, -4, -8},
+			[]int{-8, -4, -3, -2, -1},
+		},
+		{
+			"mix elements",
+			[]int{-50, -70, 0, -5, 1, 20, -2, -2, 1, 0},
+			[]int{-70, -50, -5, -2, -2, 0, 0, 1, 1, 20},
+		},
+		{
+			"identical elements",
+			[]int{2, 2, 2, 2, 2},
+			[]int{2, 2, 2, 2, 2},
+		},
+	}
 
-			sortedItems, err := selectionSort(items)
+	for _, cs := range cases {
+		t.Run(cs.name, func(t *testing.T) {
 
-			assert.EqualError(t, errorSliceIsNil, err.Error())
-			assert.Nil(t, sortedItems)
+			sortedItems, err := selectionSort(cs.arg)
+
+			assert.Nil(t, err)
+			assert.Equal(t, true, slices.Equal(cs.want, sortedItems))
 		})
-	})
-
-	t.Run("success", func(t *testing.T) {
-		t.Parallel()
-		cases := []struct {
-			name string
-			arg  []int
-			want []int
-		}{
-			{
-				"slice is empty",
-				[]int{},
-				[]int{},
-			},
-			{
-				"one element",
-				[]int{1},
-				[]int{1},
-			},
-			{
-				"two element",
-				[]int{2, 1},
-				[]int{1, 2},
-			},
-			{
-				"positive elements",
-				[]int{2, 0, 5, 1, 7, 9, 6, 3, 4, 8},
-				[]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
-			},
-			{
-				"negative elements",
-				[]int{-1, -2, -3, -4, -8},
-				[]int{-8, -4, -3, -2, -1},
-			},
-			{
-				"mix elements",
-				[]int{-50, -70, 0, -5, 1, 20, -2, -2, 1, 0},
-				[]int{-70, -50, -5, -2, -2, 0, 0, 1, 1, 20},
-			},
-			{
-				"identical elements",
-				[]int{2, 2, 2, 2, 2},
-				[]int{2, 2, 2, 2, 2},
-			},
-		}
-
-		for _, cs := range cases {
-			t.Run(cs.name, func(t *testing.T) {
-
-				sortedItems, err := selectionSort(cs.arg)
-
-				assert.Nil(t, err)
-				assert.Equal(t, true, slices.Equal(cs.want, sortedItems))
-			})
-		}
-	})
+	}
 }
--- Chapter-4/evklid-algorithm/evklidAlgorithm_test.go
+++ Chapter-4/evklid-algorithm/evklidAlgorithm_test.go
@@ -7,56 +7,54 @@ import (
 )
 
 func Test_findNOD(t *testing.T) {
-	t.Run("success", func(t *testing.T) {
-		cases := []struct {
-			arg  []int
-			want int
-		}{
-			{
-				[]int{10},
-				10,
-			},
-			{
-				[]int{7, -7},
-				7,
-			},
-			{
-				[]int{64, 48},
-				16,
-			},
-			{
-				[]int{111, -432},
-				3,
-			},
-			{
-				[]int{661, 113},
-				1,
-			},
-			{
-				[]int{48, 72, -288},
-				24,
-			},
-			{
-				[]int{78, -294, 570, 36},
-				6,
-			},
-			{
-				[]int{345, 775, 570, 95},
-				5,
-			},
-			{
-				[]int{345, 775, 1},
-				1,
-			},
-		}
+	cases := []struct {
+		arg  []int
+		want int
+	}{
+		{
+			[]int{10},
+			10,
+		},
+		{
+			[]int{7, -7},
+			7,
+		},
+		{
+			[]int{64, 48},
+			16,
+		},
+		{
+			[]int{111, -432},
+			3,
+		},
+		{
+			[]int{661, 113},
+			1,
+		},
+		{
+			[]int{48, 72, -288},
+			24,
+		},
+		{
+			[]int{78, -294, 570, 36},
+			6,
+		},
+		{
+			[]int{345, 775, 570, 95},
+			5,
+		},
+		{
+			[]int{345, 775, 1},
+			1,
+		},
+	}
 
-		for _, cs := range cases {
-			t.Run(fmt.Sprintf("NOD(%+v) = %d", cs.arg, cs.want), func(t *testing.T) {
+	for _, cs := range cases {
+		t.Run(fmt.Sprintf("NOD(%+v) = %d", cs.arg, cs.want), func(t *testing.T) {
 
-				NOD := findNOD(cs.arg...)
+			NOD := findNOD(cs.arg...)
 
-				assert.Equal(t, cs.want, NOD)
-			})
-		}
-	})
+			assert.Equal(t, cs.want, NOD)
+		})
+	}
 }
